1173cfd555df16f040c7000254d1b078ef67c4ab
Matsu1209 <rstoyo.shiho.matsuyama@gmail.com>
2023-08-09 17:58:10 +0900

    アクセス先の修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, :new]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @prototypes = Prototype.includes(:user)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:show, :new]
+  skip_before_action :authenticate_user!, only: [:show]
   def show
     user = User.find(params[:id])
     @name = user.name
